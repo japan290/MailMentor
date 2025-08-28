9a4cb5f44bb30ecb22e9ea78c786618d04c6b209
japan.nitsan <japan.nitsan@gmail.com>
2025-08-28 17:44:26 +0530

    modified config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 DATABASE_URL="postgresql://postgres:secret@localhost:5433/postgres"
 print("Using DB URL:", os.getenv("DATABASE_URL"))
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "xxx"
 OPENAI_API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 # Add or update these settings
 USE_OPENAI_API = False  # Set to False to use local models
